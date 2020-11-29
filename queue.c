db2d08b19f3d7ce33bb4dfb82ea0cd64e7fc49d3
lawrence880301 <lawrence10330869@gmail.com>
2020-11-29 22:34:00 +0800

    Change q_size return value to q->size

--- queue.c
+++ queue.c
@@ -13,7 +13,7 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
-    q->head = 2;
+    q->head = NULL;
     return q;
 }
 
@@ -84,7 +84,7 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    return q->size;
 }
 
 /*
